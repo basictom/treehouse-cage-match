3d9d84f4ae3aa1fa80a7b65dc5cc3347eae2cdff
Thomas Fitzgerald <tpfitz42@gmail.com>
2017-04-20 18:38:52 -0500

    added bower and file structure

---
+++ javascripts/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  // console.log("hello world");
+
+  const leftPlayerValue = $("#left-player").val;
+  const rightPlayerValue = $("#right-player").val;
+
+  console.log("left", leftPlayerValue);
+  console.log("right", rightPlayerValue);
+
+
+
+
+
+
+
+
+});
